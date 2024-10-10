965397b18521581c25d48cb027cae3db8a090137
temijopelo <temijeje@gmail.com>
2024-10-10 20:48:59 +0100

    added the testimonial componenet to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Hero from './components/HeroSection';
-import Navbar from './components/Navbar';
+import Hero from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import Testimonial from "./components/Testimonial";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Testimonial />
     </div>
   );
 }
