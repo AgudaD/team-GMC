61d0201574058747601869fb97ed7c06b5364d1e
temijopelo <temijeje@gmail.com>
2024-10-10 19:45:38 +0100

    created the testimonial section

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Testimonial = () => {
+  return (
+    <div>
+      <h1>Testimonial Section</h1>
+      <p>This is a Testimonial section</p>
+
+      <p className="text-right">Created by Temi.</p>
+      <p className="text-right">Created by Temi</p>
+    </div>
+  );
+};
+
+export default Testimonial;
