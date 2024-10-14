6c3e3529c23c00f4094668d185c3242c414d4f59
AgudaD <agudaoluwanifemi@gmail.com>
2024-10-14 19:09:13 +0100

    made navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,13 @@ function App() {
   return (
     <div>
       <Navbar />
-
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/organizations" element={<Organizations />} />
-      </Routes>
+      <div className="px-12 py-6">
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/organizations" element={<Organizations />} />
+        </Routes>
+      </div>
     </div>
   );
 }
