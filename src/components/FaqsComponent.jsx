4e9c790fd5b43807580bd125ed16662a072935bc
AgudaD <agudaoluwanifemi@gmail.com>
2024-10-14 20:21:25 +0100

    filled the faqs component

--- src/components/FaqsComponent.jsx
+++ src/components/FaqsComponent.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import { CaretDown } from "@phosphor-icons/react"
 
 const FaqsComponent = () => {
   return (
-    <div><h1>Frequently Asked Questions</h1>
-    <h2>General</h2>
-    <div>What is React?</div>
-    <p>How do i create React Project?</p>
-    <p>What is JSX?</p>
+    <div className="font-grotesk">
+      <h1 className="text-xl text-slate-900 font-bold">Frequently Asked Questions</h1>
+
+      <div className="mt-4 space-y-4 md:text-lg font-semibold">
+        <div className="flex items-center justify-between">
+          <p>Do you offer free trial?</p>
+          <CaretDown />
+        </div>
+        <div className="flex items-center justify-between">
+          <p>How to get future updates?</p>
+          <CaretDown />
+        </div>
+        <div className="flex items-center justify-between">
+          <p>Does this run on all devices?</p>
+          <CaretDown />
+        </div>
+        <div className="flex items-center justify-between">
+          <p>How do you manage quality control?</p>
+          <CaretDown />
+        </div>
+        <div className="flex items-center justify-between">
+          <p>Are your virtual assistants available 24/7?</p>
+          <CaretDown />
+        </div>
+      </div>
     </div>
   )
 }
