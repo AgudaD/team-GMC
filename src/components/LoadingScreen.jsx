23902531eb69c5d8a7e35def140d1d2c57765e3e
AgudaD <agudaoluwanifemi@gmail.com>
2024-10-16 17:11:00 +0100

    loading screen

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.svg"
 
 const LoadingScreen = () => {
   return (
-    <div className={`bg-slate-950 h-screen flex items-center justify-center transition-opacity duration-500`}>
+    <div className="bg-slate-950 h-screen flex items-center justify-center">
         <img src={logo} alt="" />
     </div>
   )
